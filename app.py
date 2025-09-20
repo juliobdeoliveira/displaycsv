03ca224940d86ff8334c0886512385aa8c6bd086
juliobdeoliveira <juliobdeoliveira@gmail.com>
2025-09-19 21:32:53 -0300

    Corrige estrutura do app.py para deploy no Azure

--- app.py
+++ app.py
@@ -9,23 +9,27 @@ def index():
 
 @app.route('/display', methods=['POST'])
 def display_file():
-    file = request.files['file']
-    if not file:
+    file = request.files.get('file')
+    if not file or file.filename == "":
         return "No file"
 
-    # Tenta ler o arquivo com diferentes codificações
+    df = None
     try:
         df = pd.read_csv(file, delimiter=";")
     except UnicodeDecodeError:
-        file.seek(0) # Reseta o ponteiro do arquivo
-    try:
-        df = pd.read_csv(file, encoding='latin1',
-        delimiter=";")
-    except UnicodeDecodeError:
-        file.seek(0) # Reseta o ponteiro do arquivo
-        df = pd.read_csv(file, encoding='ISO-8859-1',
-        delimiter=";")
-    # Renderiza o template com os dados do arquivo CSV
-        return render_template('display.html',tables=[df.to_html(classes='data')],titles=df.columns.values)
-        if __name__ == '__main__':
-            app.run(debug=True)
+        file.seek(0)
+        try:
+            df = pd.read_csv(file, encoding='latin1', delimiter=";")
+        except UnicodeDecodeError:
+            file.seek(0)
+            df = pd.read_csv(file, encoding='ISO-8859-1', delimiter=";")
+
+    return render_template(
+        'display.html',
+        tables=[df.to_html(classes='data', index=False)],
+        titles=df.columns.values
+    )
+
+if __name__ == '__main__':
+    # Para rodar localmente
+    app.run(host="0.0.0.0", port=8000, debug=True)
